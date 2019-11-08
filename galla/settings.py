dcea9346dcca699ccf99c1df3b76d6d392467f9f
Steve-design <smzalendo31@gmail.com>
2019-11-08 17:00:06 +0300

    update db

--- galla/settings.py
+++ galla/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'galla.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gallery',
         'USER': 'nancy',
         'PASSWORD': 'nkm',
