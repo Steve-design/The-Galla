13727d96e8ccda393cee75b99f5f2f88914882f4
Steve-design <smzalendo31@gmail.com>
2019-11-08 17:13:21 +0300

    add test for setup

--- gallery/tests.py
+++ gallery/tests.py
@@ -2,3 +2,15 @@ from django.test import TestCase
 from .models import *
 
 # Create your tests here.
+class TestImage(TestCase):
+    
+    def setUp(self):
+        self.lagos = Location.objects.create(name='Lagos')
+        self.scenery = Category.objects.create(name='scenery')
+        self.hotels = Category.objects.create(name='hotels')
+
+        self.office = Image.objects.create(
+            name='office', location=self.lagos,  description='an office picture')
+
+        self.office.category.add(self.scenery)
+        self.office.category.add(self.hotels)
