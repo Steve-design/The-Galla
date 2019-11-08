78549c2e0c077eeb3b958b5c75d4a1642475e587
Steve-design <smzalendo31@gmail.com>
2019-11-08 14:12:59 +0300

    add the urls patterns

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,3 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
+
+
+urlpatterns=[
+
+
+    url(r'^$', views.show_categories, name='imagesHomeCategory'),
+    url(r'^$', views.my_locations, name='location_results'),
+
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/', views.my_locations, name='location_results')
+
+]
