46b6ccb9e92ec9a5163adb0e5843127df9b8ad1e
Steve-design <smzalendo31@gmail.com>
2019-11-08 13:27:31 +0300

    create a function to search results

--- gallery/views.py
+++ gallery/views.py
@@ -31,7 +31,15 @@ def show_categories(request):
 
     return render(request, 'images/homepage.html', {"categories":categories,"locations":locations,  "images":images })
 
-#
-# def images(request):
-#     images=Image.all_images()
-#     return render (request, 'images/homepage.html', {"images":images})
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        image_results = Image.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'images/search.html',{"message":message,"images": image_results})
+
+    else:
+        message = "Please enter a search term"
+        return render(request, 'images/search.html',{"message":message})    
