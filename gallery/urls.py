d1ae22a7930274fe0ee77612730f962de6a1faf6
Steve-design <smzalendo31@gmail.com>
2019-11-11 11:22:39 +0300

    confiugre settings

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,12 +5,11 @@ from . import views
 
 
 urlpatterns=[
-
-
     url(r'^$', views.show_categories, name='imagesHomeCategory'),
-    # url(r'^$', views.my_locations, name='location_results'),
+    url(r'^$', views.my_locations, name='location_results'),
 
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^location/', views.my_locations, name='location_results')
-
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
