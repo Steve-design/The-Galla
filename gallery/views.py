049bff6dd07bdb29d4240f9bfb6d44fdfbfa97a8
Steve-design <smzalendo31@gmail.com>
2019-11-11 10:57:01 +0300

    update app

--- gallery/views.py
+++ gallery/views.py
@@ -4,14 +4,10 @@ from .models import *
 
 # Create your views here.
 def show_categories(request):
-    # categories=Category.all_categories()
     locations = Location.objects.all()
 
     categories = Category.objects.all()
     images = Image.all_images()
-
-    # imagescategory = Image.show_by_category(category=ca)
-
 
     if request.GET.get("category"):
         images = Image.show_by_category(request.GET.get("category"))
@@ -21,13 +17,6 @@ def show_categories(request):
 
     else:
         images= Image.all_images()
-
-
-
-    # images = Image.objects.all()
-    #
-    # if request.GET.get("category")):
-    #     images = Image.filter_by_category(request.GET.get("category"))
 
     return render(request, 'images/homepage.html', {"categories":categories,"locations":locations,  "images":images })
 
@@ -45,12 +34,9 @@ def search_results(request):
         return render(request, 'images/search.html',{"message":message})    
 
 def show_locations(request):
-    # categories=Category.all_categories()
 
     locations = Location.objects.all()
     images = Image.all_images()
-
-    # imagescategory = Image.show_by_category(category=ca)
 
 
     if request.GET.get("location"):
@@ -59,12 +45,6 @@ def show_locations(request):
     else:
         images= Image.all_images()
 
-
-
-    # images = Image.objects.all()
-    #
-    # if request.GET.get("category")):
-    #     images = Image.filter_by_category(request.GET.get("category"))
 
     return render(request, 'images/homepage.html', {"locations":locations, "images":images })
 
